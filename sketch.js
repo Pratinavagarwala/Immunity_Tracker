59b894ad0f44e438b8acb5e6303c05019c0a40a8
Pratinavagarwala <66483787+Pratinavagarwala@users.noreply.github.com>
2020-08-23 23:08:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var canvas;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function preload(){
+  immunity=loadImage("images/immunity_1300.png");
+}
+
+
+function draw(){
+  background(immunity);
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
